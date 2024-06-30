bf5f7ddd94dd71fabf6ba7f3f8349adff0143112
Terfa John <148336112+CyberSage5@users.noreply.github.com>
2024-06-30 21:32:44 +0000

    initial

---
+++ app.js
@@ -0,0 +1,12 @@
+function updateTime() {
+    const now = new Date();
+    const utcTime = now.toUTCString().split(' ')[4];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const day = days[now.getUTCDay()];
+
+    document.querySelector('[data-testid="currentTimeUTC"]').textContent = `Current Time (UTC): ${utcTime}`;
+    document.querySelector('[data-testid="currentDay"]').textContent = `Current Day: ${day}`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
